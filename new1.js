af33f8ce8a75e4b00f936698735da8a45fa09461
jagadish-4125 <jagadeeshm572@gmail.com>
2025-09-11 13:52:50 +0530

    first commit

---
+++ new1.js
@@ -0,0 +1,85 @@
+// const givenName="jagadish";
+// //console.log(givenName);
+// const rollno="234g1a3355";
+// //console.log(rollno);
+// const branch="CSE";
+// //console.log(branch);
+// const course="Full stack web development";
+// //console.log(course);
+
+// //function
+// function generateWelcomeMsg(name){
+//     return "Welcome " + name + " good morning";
+// }
+
+// let Welcome_msg=generateWelcomeMsg(givenName);
+// console.log(Welcome_msg);
+
+// // example
+// function mybio(name,rollno,branch,course){
+//     return "my name is   :" + name +"\nmy rollno is :" + rollno +"\nmy branch is :" + branch +"\nmy course is :" + course;
+// }
+// let my_bio=mybio(givenName,rollno,branch,course);
+// console.log(my_bio);
+
+// // example2
+// function add(a,b){
+//     a=10;
+//     b=20;
+//     return a+b;
+// }
+// console.log(add());
+
+// // example3
+
+// let age=20;
+
+// let addage=age+2;
+// let subage=age-2;
+// let mulage=age*2;
+// let divage=age/2;
+
+// console.log(addage);
+// console.log(subage);
+// console.log(mulage);
+// console.log(divage);
+
+// // example4
+
+// let score=20;
+
+// function updatescore(){
+//     return [score+10,score-10,score*10,score/10];
+// }
+
+// console.log(updatescore());
+
+// function area(a,b){
+//     a=10;
+//     b=20;
+//     return a*b;
+// }
+// console.log(area());
+
+// //example
+// function factorial(n) {
+//     let fact = 1;
+//     for (let i = 1; i <= n; i++) {
+//         fact *= i;
+//     }
+//     return fact;
+// }
+
+// console.log(factorial(5));
+
+// example6
+
+let a=10;
+let b=20;
+ function calulator(a,b){
+    console.log("addition is :" + (a+b));
+    console.log("subtraction is :" + (a-b));
+    console.log("multiplication is :" + (a*b));
+    console.log("division is :" + (a/b));
+ }
+ calulator(a,b);  
