1a06a5b59002498e508f4302806cc1c91c532a67
jagadish-4125 <jagadeeshm572@gmail.com>
2025-09-11 14:29:02 +0530

    first commit

--- new1.js
+++ new1.js
@@ -1,76 +1,75 @@
-// const givenName="jagadish";
-// //console.log(givenName);
-// const rollno="234g1a3355";
-// //console.log(rollno);
-// const branch="CSE";
-// //console.log(branch);
-// const course="Full stack web development";
-// //console.log(course);
+const givenName="jagadish";
+//console.log(givenName);
+const rollno="234g1a3355";
+//console.log(rollno);
+const branch="CSE";
+//console.log(branch);
+const course="Full stack web development";
+//console.log(course);
 
-// //function
-// function generateWelcomeMsg(name){
-//     return "Welcome " + name + " good morning";
-// }
+//function
+function generateWelcomeMsg(name){
+    return "Welcome " + name + " good morning";
+}
 
-// let Welcome_msg=generateWelcomeMsg(givenName);
-// console.log(Welcome_msg);
+let Welcome_msg=generateWelcomeMsg(givenName);
+console.log(Welcome_msg);
 
-// // example
-// function mybio(name,rollno,branch,course){
-//     return "my name is   :" + name +"\nmy rollno is :" + rollno +"\nmy branch is :" + branch +"\nmy course is :" + course;
-// }
-// let my_bio=mybio(givenName,rollno,branch,course);
-// console.log(my_bio);
+// example
+function mybio(name,rollno,branch,course){
+    return "my name is   :" + name +"\nmy rollno is :" + rollno +"\nmy branch is :" + branch +"\nmy course is :" + course;
+}
+let my_bio=mybio(givenName,rollno,branch,course);
+console.log(my_bio);
 
-// // example2
-// function add(a,b){
-//     a=10;
-//     b=20;
-//     return a+b;
-// }
-// console.log(add());
+// example2
+function add(a,b){
+    a=10;
+    b=20;
+    return a+b;
+}
+console.log(add());
 
-// // example3
+// example3
 
-// let age=20;
+let age=20;
 
-// let addage=age+2;
-// let subage=age-2;
-// let mulage=age*2;
-// let divage=age/2;
+let addage=age+2;
+let subage=age-2;
+let mulage=age*2;
+let divage=age/2;
 
-// console.log(addage);
-// console.log(subage);
-// console.log(mulage);
-// console.log(divage);
+console.log(addage);
+console.log(subage);
+console.log(mulage);
+console.log(divage);
 
-// // example4
+// example4
 
-// let score=20;
+let score=20;
 
-// function updatescore(){
-//     return [score+10,score-10,score*10,score/10];
-// }
+function updatescore(){
+    return [score+10,score-10,score*10,score/10];
+}
 
-// console.log(updatescore());
+console.log(updatescore());
 
-// function area(a,b){
-//     a=10;
-//     b=20;
-//     return a*b;
-// }
-// console.log(area());
+function area(a,b){
+    a=10;
+    b=20;
+    return a*b;
+}
+console.log(area());
 
-// //example
-// function factorial(n) {
-//     let fact = 1;
-//     for (let i = 1; i <= n; i++) {
-//         fact *= i;
-//     }
-//     return fact;
-// }
-
-// console.log(factorial(5));
+ //example
+function factorial(m) {
+    if (m==0||m==1){
+      return 1;
+}else{
+   return m * factorial(m-1);
+   }
+}
+console.log(factorial(5));
 
 // example6
 
@@ -83,3 +82,18 @@ let b=20;
     console.log("division is :" + (a/b));
  }
  calulator(a,b);  
+
+ calulator(0,2)
+
+//example7 compound interest
+let p=2000;
+let r=10;
+let t=2;
+function compoundinterest(p,r,t){
+   let amount=p*(Math.pow((1+r/100),t));
+   let ci=amount-p;
+   return ci;
+}
+console.log(compoundinterest(p,r,t));
+
+console.log(compoundinterest(3000,5,3));
